326e0f3e74819bfd40620658cc40bc2b3859945f
dchouhan <dchouhan@qodeleaf.com>
2023-10-26 15:28:52 +0530

    feat: add count parameter in task

--- scripts/task.ts
+++ scripts/task.ts
@@ -1,18 +1,17 @@
 import { task } from "hardhat/config";
 
-task("balances", "Prints the balances of all the addresses!").addParam("contract", "contract address").setAction(async (arg) => {
+task("balances", "Prints the balances of all the addresses!")
+    .addParam("contract", "contract address")
+    .addParam("count", "number of addresses to query!")
+    .setAction(async (arg) => {
 
-    const [address1, address2, address3, address4] = await ethers.getSigners()
+        const signers: Array<any> = await ethers.getSigners()
+        const contract = await ethers.getContractAt("ERC20Token", arg.contract)
 
-    const contract = await ethers.getContractAt("ERC20Token", arg.contract)
+        for (const signer of signers.slice(0, arg.count)) {
+            const balance = await contract.balanceOf(signer.address)
+            console.log(signer.address, "-", balance);
+        }
 
-    const balanceOf1 = await contract.balanceOf(address1.address)
-    const balanceOf2 = await contract.balanceOf(address2.address)
-    const balanceOf3 = await contract.balanceOf(address3.address)
-    const balanceOf4 = await contract.balanceOf(address4.address)
+    })
 
-    console.log("address1 - ", balanceOf1);
-    console.log("address2 - ", balanceOf2);
-    console.log("address3 - ", balanceOf3);
-    console.log("address4 - ", balanceOf4);
-})
